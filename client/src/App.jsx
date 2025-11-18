74f54cbc5bef43ff8ca0968c5adb3e9cd374304d
akash <akashtenp@gmail.com>
2025-11-18 05:47:43 +0000

    project setup>done

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+    return (
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+             <Button>Click me</Button>
+        </h1>
+    )
+}
+
+export default App
+
+
+
+
